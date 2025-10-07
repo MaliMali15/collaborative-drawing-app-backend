1e629930f43d07fc5f036cf2b7b188f62305c1d7
Bhavesh Mali <bhaveshmali2304@gmail.com>
2025-10-08 01:27:52 +0530

    install socket.io for real-time communication and implement user routes

--- src/app.js
+++ src/app.js
@@ -14,20 +14,9 @@ app.use(express.urlencoded({extended:true,limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import userRouter from "./routes/user.routes.js"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/user", userRouter)
 
 
 export default app
---
+++ src/routes/user.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { jwtVerify } from "../middlewares/auth.middleware.js";
+import {registerUser,userLogin,userLogout,tokenRefresher,getCurrentUser,changePassword,updateAccountDetails} from "../controllers/user.controller.js"
+
+
+const router = Router()
+
+router.route("/loging").post(userLogin)
+router.route("/logout").post(jwtVerify,userLogout)
+router.route("/register").post(registerUser)
+router.route("/refreshTokens").post(tokenRefresher)
+router.route("/current-user").get(jwtVerify, getCurrentUser)
+router.route("/change-password").post(jwtVerify, changePassword)
+router.route("/updateUserInfo").patch(jwtVerify,updateAccountDetails)
+
+export default router
